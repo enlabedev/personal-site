e7fe33e56b614ec1d1906fe1cd76c674c6806519
Enrique Lazo Bello <enlabe@gmail.com>
2024-03-07 16:36:34 -0500

    Lazy on Load

--- script.js
+++ script.js
@@ -68,6 +68,11 @@ const Site = (() => {
       });
   
     }
+
+    const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+    lazyImages.forEach(img => {
+      img.src = img.dataset.src;
+    });
     
   };
 
